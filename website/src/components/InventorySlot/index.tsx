50860a6b9145c15d4693386f43bf0fc73738fe9f
Abbysssal <55982389+Abbysssal@users.noreply.github.com>
2021-08-09 20:37:39 +0700

    fix hook inconsistency

--- website/src/components/InventorySlot/index.tsx
+++ website/src/components/InventorySlot/index.tsx
@@ -9,36 +9,42 @@ export type Props = {
   count?: string | number,
   countColor?: string,
 
-  type?: "normal" | "selected" | "locked",
+  type?: "normal" | "selected" | "locked" | null,
   onClick?: () => void,
   hoverable?: boolean,
   
   uid?: string,
 }
 
-export default function ({sprite, tooltip, tooltipColor, count, countColor, hoverable, type, onClick}: Props): JSX.Element {
+export default function ({sprite, tooltip, tooltipColor, count, countColor, hoverable, type = "normal", onClick}: Props): JSX.Element {
 
-  type = type || "normal";
+  const slotSrc = type ? useBaseUrl(`/img/components/InventorySlot/${type}.png`) : undefined;
+  const slotHoverSrc = type ? useBaseUrl(`/img/components/InventorySlot/${type}Hover.png`) : undefined;
+  const opacity = type == "locked" ? 0.15625 : 1;
 
   return (
     <div className={styles.container} onClick={onClick} style={{cursor: hoverable ? "pointer" : "inherit"}}>
-      {hoverable &&
-        <img key="hoverslot" className={styles.hoverslot} src={useBaseUrl(`/img/components/InventorySlot/${type}Hover.png`)}/>
+      {type &&
+        <>
+          {hoverable &&
+            <img key="hoverslot" className={styles.hoverslot} src={slotHoverSrc}/>
+          }
+          <img key="slot" className={styles.slot} src={slotSrc}/>
+        </>
       }
-      <img key="slot" className={styles.slot} src={useBaseUrl(`/img/components/InventorySlot/${type}.png`)}/>
 
       {sprite &&
-        <img key="sprite" style={{opacity: type == "locked" ? 0.15625 : 1}} className={styles.sprite} src={sprite}/>
+        <img key="sprite" style={{opacity}} className={styles.sprite} src={sprite}/>
       }
 
       {tooltip &&
-        <span key="tooltip" className={styles.tooltip} style={{color: tooltipColor || "#FFED00"}}>
+        <span key="tooltip" className={styles.tooltip} style={{opacity, color: tooltipColor || "#FFED00"}}>
           {tooltip}
         </span>
       }
 
       {count &&
-        <span key="count" className={styles.count} style={{color: countColor || "#FFFFFF"}}>
+        <span key="count" className={styles.count} style={{opacity, color: countColor || "#FFFFFF"}}>
           {count}
         </span>
       }
