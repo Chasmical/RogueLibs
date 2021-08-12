8b9c815f5884523e293225a427572ef19300d552
Abbysssal <55982389+Abbysssal@users.noreply.github.com>
2021-08-12 10:44:09 +0700

    refactor with SASS

--- website/src/components/InventorySlot/index.tsx
+++ website/src/components/InventorySlot/index.tsx
@@ -1,20 +1,7 @@
 import React, { useState } from 'react';
 import clsx from 'clsx';
 import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './index.module.css';
-
-import normal from './assets/normal.png';
-import normalHover from './assets/normalHover.png';
-import selected from './assets/selected.png';
-import selectedHover from './assets/selectedHover.png';
-import locked from './assets/locked.png';
-import lockedHover from './assets/lockedHover.png';
-
-const images: {readonly [type: string]: [slot: string, slotHover: string]} = {
-  normal: [normal, normalHover],
-  selected: [selected, selectedHover],
-  locked: [locked, lockedHover],
-}
+import styles from './index.module.scss';
 
 export type Props = {
   sprite?: string,
@@ -33,34 +20,23 @@ export type Props = {
 
 export default function ({sprite, tooltip, tooltipColor, count, countColor, hoverable, cantClick, type = "normal", onClick}: Props): JSX.Element {
 
-  const [slotSrc, slotHoverSrc] = type ? images[type] : [];
-  const opacity = type == "locked" ? 0.15625 : 1;
-
-  const cursor = cantClick ? styles.cursorLocked : styles.cursorNormal;
-
   return (
-    <div onClick={onClick} className={clsx(styles.container, cursor)}>
-      {type &&
-        <>
-          {hoverable &&
-            <img key="hoverslot" width={80} className={styles.hoverslot} src={slotHoverSrc}/>
-          }
-          <img key="slot" width={80} className={styles.slot} src={slotSrc}/>
-        </>
-      }
-
+    <div onClick={onClick} className={clsx(
+      styles.container,
+      cantClick && styles.cursorLocked,
+      type && styles[type],
+      hoverable && styles.hoverable,
+    )}>
       {sprite &&
-        <img key="sprite" style={{opacity}} className={styles.sprite} src={sprite}/>
+        <img key="sprite" className={styles.sprite} src={sprite}/>
       }
-
       {tooltip &&
-        <span key="tooltip" className={styles.tooltip} style={{opacity, color: tooltipColor || "#FFED00"}}>
+        <span key="tooltip" className={styles.tooltip} style={{color: tooltipColor || "#FFED00"}}>
           {tooltip}
         </span>
       }
-
       {count &&
-        <span key="count" className={styles.count} style={{opacity, color: countColor || "#FFFFFF"}}>
+        <span key="count" className={styles.count} style={{color: countColor || "#FFFFFF"}}>
           {count}
         </span>
       }
