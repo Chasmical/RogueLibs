7f94007124588af09267dc4104b87a42daad5ba7
Abbysssal <55982389+Abbysssal@users.noreply.github.com>
2021-12-30 18:42:27 +0700

    upgrade docusaurus

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -5,21 +5,21 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
-import useThemeContext from '@theme/hooks/useThemeContext';
+import useThemeContext from '@theme-original/hooks/useThemeContext';
 import Logo from '@site/static/img/logo.png';
 import LogoDark from '@site/static/img/logo-inverted.png';
 import LogoPokemon from '@site/static/img/logo-pokemon.png';
 import Translate from '@docusaurus/Translate';
-import { useParams } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { parse as queryParse } from 'query-string';
 
 function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
   const { isDarkTheme } = useThemeContext();
-  const { pokemon } = useParams() as any;
+  const params = queryParse(useLocation().search);
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <img src={pokemon != undefined ? LogoPokemon : isDarkTheme ? LogoDark : Logo} width='50%'/>
+        <img src={params.pokemon !== undefined ? LogoPokemon : isDarkTheme ? LogoDark : Logo} width='50%'/>
         <p className="hero__subtitle">
           <Translate id="homepage.tagline">
             Doing the impossible.
