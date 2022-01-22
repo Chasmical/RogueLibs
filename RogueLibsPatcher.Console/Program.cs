191d8147332fd45b584f9726f37cf92916d7803e
Abbysssal <55982389+Abbysssal@users.noreply.github.com>
2022-01-22 09:22:41 +0700

    fix something with patchers

--- RogueLibsPatcher.Console/Program.cs
+++ RogueLibsPatcher.Console/Program.cs
@@ -11,12 +11,38 @@ namespace RogueLibsPatcher.Console
 			string path = System.Console.ReadLine();
 			path = path.Trim('"');
 			string newPath = path + "2";
-			AssemblyDefinition assembly = AssemblyDefinition.ReadAssembly(path);
+			AssemblyDefinition assembly = AssemblyDefinition.ReadAssembly(path, new ReaderParameters()
+            {
+				AssemblyResolver = new CustomResolver(),
+            });
 			RogueLibsPatcher.Patch(assembly);
-			assembly.Write(newPath);
+            assembly.Write(newPath);
 			assembly.Dispose();
 			File.Copy(newPath, path, true);
 			File.Delete(newPath);
 		}
+        private class CustomResolver : BaseAssemblyResolver
+        {
+            private readonly DefaultAssemblyResolver _defaultResolver;
+
+            public CustomResolver()
+            {
+                _defaultResolver = new DefaultAssemblyResolver();
+            }
+
+            public override AssemblyDefinition Resolve(AssemblyNameReference name)
+            {
+                AssemblyDefinition assembly;
+                try
+                {
+                    assembly = _defaultResolver.Resolve(name);
+                }
+                catch (AssemblyResolutionException ex)
+                {
+                    assembly = AssemblyDefinition.ReadAssembly(@"D:\Steam\steamapps\common\Streets of Rogue\StreetsOfRogue_Data\Managed\XGamingRuntime.dll");
+                }
+                return assembly;
+            }
+        }
 	}
 }
