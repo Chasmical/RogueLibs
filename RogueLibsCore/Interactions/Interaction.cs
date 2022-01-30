9ba55a3b9936fd76393e012cf11c5a2585811bfd
Abbysssal <55982389+Abbysssal@users.noreply.github.com>
2022-01-30 18:56:50 +0700

    :alembic: Experiment with interactions

---
+++ RogueLibsCore/Interactions/Interaction.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace RogueLibsCore
+{
+	public abstract class Interaction
+	{
+		public InteractionModel Model { get; internal set; }
+        public PlayfieldObject Object => Model.Object;
+        public Agent Agent => Model.Agent;
+        public InteractionHelper Helper => Model.Helper;
+
+        public virtual bool ImplicitAction => false;
+
+		public string ButtonName { get; private set; }
+		public int? ButtonPrice { get; private set; }
+		public string ButtonExtra { get; private set; }
+
+        protected bool SetButton(string buttonName)
+            => SetButton(buttonName, null, null);
+        protected bool SetButton(string buttonName, string buttonExtra)
+            => SetButton(buttonName, null, buttonExtra);
+        protected bool SetButton(string buttonName, int? buttonPrice)
+            => SetButton(buttonName, buttonPrice, null);
+        protected bool SetButton(string buttonName, int? buttonPrice, string buttonExtra)
+        {
+            ButtonName = buttonName;
+            ButtonPrice = buttonPrice;
+            ButtonExtra = buttonExtra;
+            return true;
+        }
+
+        protected void StopInteraction() => Model.StopInteraction();
+        protected bool SetCallback(Action callback)
+        {
+            if (Model.CancelCallback is null)
+                Model.CancelCallback = callback;
+            return false;
+        }
+
+        public abstract bool SetupButton();
+        public abstract void OnPressed();
+        public virtual void OnPressedImplicitly() => OnPressed();
+
+        public virtual void OnOpen() { }    // TODO
+		public virtual void WhileOpen() { } // TODO
+		public virtual void OnClose() { }   // TODO
+
+        public virtual void OnMouseEnter() { } // TODO
+		public virtual void OnMouseOver() { }  // TODO
+		public virtual void OnMouseExit() { }  // TODO
+
+    }
+    public abstract class CustomInteraction : Interaction
+    {
+        public abstract bool MatchObject(InteractionModel model);
+    }
+    public abstract class CustomInteraction<T> : CustomInteraction where T : PlayfieldObject
+    {
+        public new InteractionModel<T> Model => (InteractionModel<T>)base.Model;
+        public new T Object => Model.Object;
+
+        public sealed override bool MatchObject(InteractionModel model)
+            => model is InteractionModel<T> tModel && MatchObject(tModel);
+        public virtual bool MatchObject(InteractionModel<T> model) => true;
+
+    }
+    public sealed class SimpleInteraction : CustomInteraction
+    {
+        public SimpleInteraction(string name, int? price, string extra, bool implicitAction, Action<InteractionModel> action)
+        {
+            _name = name;
+            _price = price;
+            _extra = extra;
+            ImplicitAction = implicitAction;
+            _action = action;
+        }
+
+        private readonly string _name;
+        private readonly int? _price;
+        private readonly string _extra;
+        public override bool ImplicitAction { get; }
+        private readonly Action<InteractionModel> _action;
+
+        public override bool MatchObject(InteractionModel model) => true;
+        public override bool SetupButton() => SetButton(_name, _price, _extra);
+        public override void OnPressed() => _action(Model);
+
+    }
+    public sealed class SimpleInteraction<T> : CustomInteraction<T> where T : PlayfieldObject
+    {
+        public SimpleInteraction(string name, int? price, string extra, bool implicitAction, Action<InteractionModel<T>> action)
+        {
+            _name = name;
+            _price = price;
+            _extra = extra;
+            ImplicitAction = implicitAction;
+            _action = action;
+        }
+
+        private readonly string _name;
+        private readonly int? _price;
+        private readonly string _extra;
+        public override bool ImplicitAction { get; }
+        private readonly Action<InteractionModel<T>> _action;
+
+        public override bool SetupButton() => SetButton(_name, _price, _extra);
+        public override void OnPressed() => _action(Model);
+
+    }
+    public interface IInteractionProvider
+    {
+        IEnumerable<Interaction> GetInteractions(InteractionModel model);
+    }
+    public class SimpleInteractionProvider : IInteractionProvider
+    {
+        public SimpleInteractionProvider(Action<SimpleInteractionProvider> handler) => _handler = handler;
+
+        private readonly Action<SimpleInteractionProvider> _handler;
+        private readonly List<Interaction> interactions = new List<Interaction>();
+
+        private void EnsureModel()
+        {
+			if (_model is null) throw new InvalidOperationException(
+                "SimpleInteractionProvider<T> instance must only be accessed in the handler delegate."
+                + " In button callbacks use the passed parameter to access associated properties.");
+        }
+
+        public SimpleInteraction AddButton(string buttonName, Action<InteractionModel> action)
+            => AddButton(buttonName, null, null, action);
+        public SimpleInteraction AddButton(string buttonName, string buttonExtra, Action<InteractionModel> action)
+            => AddButton(buttonName, null, buttonExtra, action);
+        public SimpleInteraction AddButton(string buttonName, int? buttonPrice, Action<InteractionModel> action)
+            => AddButton(buttonName, buttonPrice, null, action);
+        public SimpleInteraction AddButton(string buttonName, int? buttonPrice, string buttonExtra, Action<InteractionModel> action)
+        {
+            EnsureModel();
+            SimpleInteraction interaction = new SimpleInteraction(buttonName, buttonPrice, buttonExtra, false, action);
+            interactions.Add(interaction);
+            return interaction;
+        }
+
+        public SimpleInteraction AddImplicitButton(string buttonName, Action<InteractionModel> action)
+            => AddImplicitButton(buttonName, null, null, action);
+        public SimpleInteraction AddImplicitButton(string buttonName, string buttonExtra, Action<InteractionModel> action)
+            => AddImplicitButton(buttonName, null, buttonExtra, action);
+        public SimpleInteraction AddImplicitButton(string buttonName, int? buttonPrice, Action<InteractionModel> action)
+            => AddImplicitButton(buttonName, buttonPrice, null, action);
+        public SimpleInteraction AddImplicitButton(string buttonName, int? buttonPrice, string buttonExtra, Action<InteractionModel> action)
+        {
+            EnsureModel();
+            SimpleInteraction interaction = new SimpleInteraction(buttonName, buttonPrice, buttonExtra, true, action);
+            interactions.Add(interaction);
+            return interaction;
+        }
+
+        public Action SetStopCallback(Action<InteractionModel> stopCallback)
+        {
+            EnsureModel();
+            InteractionModel model = Model;
+            void Callback() => stopCallback(model);
+            if (model.CancelCallback is null)
+                model.CancelCallback = Callback;
+            return Callback;
+        }
+
+        private InteractionModel _model;
+        public InteractionModel Model
+        {
+            get
+            {
+                EnsureModel();
+                return _model;
+            }
+        }
+        public PlayfieldObject Object => Model.Object;
+        public Agent Agent => Model.Agent;
+        public InteractionHelper Helper => Model.Helper;
+        // ReSharper disable once InconsistentNaming
+		[SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "That's how it's called in the original code")]
+        public GameController gc => GameController.gameController;
+
+        IEnumerable<Interaction> IInteractionProvider.GetInteractions(InteractionModel model)
+        {
+            interactions.Clear();
+            try
+            {
+                _model = model;
+                _handler(this);
+            }
+            catch (Exception e)
+            {
+                RogueFramework.LogError($"SimpleInteractionProvider's handler on {Object} threw an exception.");
+                RogueFramework.LogError(e.ToString());
+            }
+            finally
+            {
+                _model = null;
+            }
+            return interactions;
+        }
+
+    }
+    public class SimpleInteractionProvider<T> : IInteractionProvider where T : PlayfieldObject
+    {
+        public SimpleInteractionProvider(Action<SimpleInteractionProvider<T>> handler) => _handler = handler;
+
+        private readonly Action<SimpleInteractionProvider<T>> _handler;
+        private readonly List<Interaction> interactions = new List<Interaction>();
+
+        private void EnsureModel()
+        {
+			if (_model is null) throw new InvalidOperationException(
+                "SimpleInteractionProvider<T> instance must only be accessed in the handler delegate."
+                + " In button callbacks use the passed parameter to access associated properties.");
+        }
+
+        public SimpleInteraction<T> AddButton(string buttonName, Action<InteractionModel<T>> action)
+            => AddButton(buttonName, null, null, action);
+        public SimpleInteraction<T> AddButton(string buttonName, string buttonExtra, Action<InteractionModel<T>> action)
+            => AddButton(buttonName, null, buttonExtra, action);
+        public SimpleInteraction<T> AddButton(string buttonName, int? buttonPrice, Action<InteractionModel<T>> action)
+            => AddButton(buttonName, buttonPrice, null, action);
+        public SimpleInteraction<T> AddButton(string buttonName, int? buttonPrice, string buttonExtra, Action<InteractionModel<T>> action)
+        {
+            EnsureModel();
+            SimpleInteraction<T> interaction = new SimpleInteraction<T>(buttonName, buttonPrice, buttonExtra, false, action);
+            interactions.Add(interaction);
+            return interaction;
+        }
+
+        public SimpleInteraction<T> AddImplicitButton(string buttonName, Action<InteractionModel<T>> action)
+            => AddImplicitButton(buttonName, null, null, action);
+        public SimpleInteraction<T> AddImplicitButton(string buttonName, string buttonExtra, Action<InteractionModel<T>> action)
+            => AddImplicitButton(buttonName, null, buttonExtra, action);
+        public SimpleInteraction<T> AddImplicitButton(string buttonName, int? buttonPrice, Action<InteractionModel<T>> action)
+            => AddImplicitButton(buttonName, buttonPrice, null, action);
+        public SimpleInteraction<T> AddImplicitButton(string buttonName, int? buttonPrice, string buttonExtra, Action<InteractionModel<T>> action)
+        {
+            EnsureModel();
+            SimpleInteraction<T> interaction = new SimpleInteraction<T>(buttonName, buttonPrice, buttonExtra, true, action);
+            interactions.Add(interaction);
+            return interaction;
+        }
+
+        public Action SetStopCallback(Action<InteractionModel<T>> stopCallback)
+        {
+            EnsureModel();
+            InteractionModel<T> model = Model;
+            void Callback() => stopCallback(model);
+            if (model.CancelCallback is null)
+                model.CancelCallback = Callback;
+            return Callback;
+        }
+
+        private InteractionModel<T> _model;
+        public InteractionModel<T> Model
+        {
+            get
+            {
+                EnsureModel();
+                return _model;
+            }
+        }
+        public T Object => Model.Object;
+        public Agent Agent => Model.Agent;
+        public InteractionHelper Helper => Model.Helper;
+        // ReSharper disable once InconsistentNaming
+		[SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "That's how it's called in the original code")]
+        public GameController gc => GameController.gameController;
+
+        IEnumerable<Interaction> IInteractionProvider.GetInteractions(InteractionModel model)
+        {
+            if (model is InteractionModel<T> tModel)
+            {
+                interactions.Clear();
+                try
+                {
+                    _model = tModel;
+                    _handler(this);
+                }
+                catch (Exception e)
+                {
+                    RogueFramework.LogError($"SimpleInteractionProvider's handler on {Object} threw an exception.");
+                    RogueFramework.LogError(e.ToString());
+                }
+                finally
+                {
+                    _model = null;
+                }
+                return interactions;
+            }
+            return Enumerable.Empty<Interaction>();
+        }
+
+    }
+}
--- RogueLibsCore/Patches/Patches_Interactions.cs
+++ RogueLibsCore/Patches/Patches_Interactions.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace RogueLibsCore
 {
@@ -20,76 +22,289 @@ namespace RogueLibsCore
                 Patcher.Prefix(typeof(T), nameof(PlayfieldObject.DetermineButtons), nameof(DetermineButtonsHook));
                 Patcher.Prefix(typeof(T), nameof(PlayfieldObject.PressedButton), nameof(PressedButtonHook), parameterTypes);
             }
+            void PatchInteract<T>() where T : PlayfieldObject
+            {
+                Patcher.Prefix(typeof(T), nameof(PlayfieldObject.Interact), nameof(InteractHook));
+            }
+            void PatchInteractFar<T>() where T : PlayfieldObject
+            {
+                Patcher.Prefix(typeof(T), nameof(PlayfieldObject.InteractFar), nameof(InteractFarHook));
+            }
+
+            RogueInteractions.CreateProvider(h =>
+            {
+                if (!h.Helper.interactingFar) return;
+                if (h.Object is AlarmButton)
+                {
+                    if (h.Agent.oma.superSpecialAbility && h.Agent.agentName == "Hacker"
+                        || h.Agent.statusEffects.hasTrait("HacksBlowUpObjects"))
+                    {
+                        h.AddButton("HackExplode", m => (m.Object as ObjectReal)?.HackExplode(m.Agent));
+                    }
+                }
+            });
+            RogueInteractions.CreateProvider(h =>
+            {
+                if (h.Object is ObjectReal objReal && objReal.CanCollectAlienPart())
+                {
+                    h.AddButton("CollectPart", m =>
+                    {
+                        m.Object.StartCoroutine(m.Object.Operating(m.Agent, null, 5f, true, "Collecting"));
+                        if (!m.Agent.statusEffects.hasTrait("OperateSecretly") && m.Object.functional)
+                        {
+                            m.gc.spawnerMain.SpawnNoise(m.Object.tr.position, 1f, m.Agent, "Normal", m.Agent);
+                            m.gc.audioHandler.Play(m.Object, "Hack");
+                            (m.Object as ObjectReal)?.SpawnParticleEffect("Hack", m.Object.tr.position);
+                            m.gc.spawnerMain.SpawnStateIndicator(m.Object, "HighVolume");
+                            m.gc.OwnCheck(m.Agent, m.Object.go, "Normal", 0);
+                        }
+                    });
+                }
+            });
+
+            Patch<AirConditioner>(params2);
+            PatchInteract<AirConditioner>();
+            RogueInteractions.CreateProvider<AirConditioner>(h =>
+            {
+                if (h.Helper.interactingFar) return;
+                if (h.gc.gasesList.Exists(g => g.startingChunk == h.Object.startingChunk))
+                {
+                    h.SetStopCallback(m => m.Agent.SayDialogue("AlreadyGassing"));
+                    return;
+                }
+                if (!h.Object.isBroken())
+                {
+                    if (h.Agent.inventory.InvItemList.Exists(item => h.Object.playerHasUsableItem(item)))
+                        h.AddButton("InsertItem", m => m.Object.ShowUseOn("InsertItem"));
+                    else h.SetStopCallback(m => m.Agent.SayDialogue("CantUseAirConditioner"));
+                }
+            });
+
+            Patch<AlarmButton>(params2);
+            PatchInteract<AlarmButton>();
+            RogueLibs.CreateCustomName("PressAlarmButton", NameTypes.Interface, new CustomNameInfo
+            {
+                English = "Press",
+                Russian = "Нажать",
+            });
+            RogueInteractions.CreateProvider<AlarmButton>(h =>
+            {
+                if (h.Helper.interactingFar)
+                {
+                    if (!h.Object.hacked)
+                    {
+                        h.AddButton("AllAccessAlarmButton", m =>
+                        {
+                            h.Object.hacked = true;
+                            if (!h.gc.serverPlayer) h.gc.playerAgent.objectMult.ObjectAction(h.Object.objectNetID, "AllAccess");
+                        });
+                    }
+                }
+                else if (!h.Object.isBroken())
+                {
+                    h.AddImplicitButton("PressAlarmButton", m =>
+                    {
+                        h.Object.lastHitByAgent = h.Object.interactingAgent;
+                        if (h.Agent.upperCrusty || h.Object.hacked)
+                            h.Object.ToggleSwitch(h.Agent, null);
+                        else h.Object.Say("CantUseAlarmButton");
+                    });
+                }
+            });
+
+            Patch<Altar>(params2);
+            PatchInteract<Altar>();
+            RogueInteractions.CreateProvider<Altar>(h =>
+            {
+                if (h.Helper.interactingFar) return;
+                h.AddButton("MakeOffering", m =>
+                {
+                    if (m.Object.offeringsMade >= m.Object.offeringLimit)
+                    {
+                        m.gc.audioHandler.Play(m.Object, "CantDo");
+                        m.Object.StopInteraction();
+                        return;
+                    }
+                    m.Agent.SayDialogue("OfferingMustBeInBuilding");
+                    m.Object.commander = m.Agent;
+                    m.Agent.mainGUI.invInterface.ShowTarget(m.Object, "MakeOffering");
+                    m.Object.StartCoroutine("MakingOffer");
+                });
+            });
+
+            Patch<AmmoDispenser>(params2);
+            PatchInteract<AmmoDispenser>();
+            RogueInteractions.CreateProvider<AmmoDispenser>(h =>
+            {
+                if (h.Helper.interactingFar)
+                {
+                    if (h.Object.hacked == 0)
+                    {
+                        h.AddButton("ReduceAmmoPrices", m =>
+                        {
+                            m.gc.audioHandler.Play(m.Agent, "Success");
+                            m.Object.ReduceAmmoPrices(m.Agent);
+                            m.StopInteraction();
+                        });
+                    }
+                }
+                else if (!h.Object.isBrokenShowButtons())
+                {
+                    h.SetStopCallback(m => m.Agent.SayDialogue("CantUseAmmoDispenser"));
+
+                    if (h.Agent.inventory.InvItemList.Exists(i => i.itemType == ItemTypes.WeaponProjectile) || h.Agent.mechFilled
+                        || h.Agent.bigQuest == "Alien" && h.Agent.oma.bigQuestObjectCount < 3 && !h.Agent.interactionHelper.interactingFar
+                        && !h.gc.loadLevel.LevelContainsMayor())
+                    {
+                        h.AddButton("RefillGun", m => m.Object.ShowUseOn("RefillGun"));
+                    }
+					if (h.Agent.statusEffects.hasTrait("OilRestoresHealth"))
+                    {
+                        InvItem invItem = new InvItem { invItemName = "OilContainer", invItemCount = 1 };
+                        invItem.ItemSetup(false);
+
+                        float costMultiplier = h.Agent.statusEffects.hasTrait("OilRestoresMoreHealth")
+                                               || h.Agent.oma.superSpecialAbility ? 3f : 1.5f;
+                        invItem.itemValue = (int)(invItem.itemValue / costMultiplier);
+                        float currentHealthCost = h.Agent.health / invItem.initCount * invItem.itemValue;
+                        int healCost = Mathf.Clamp(h.Object.determineMoneyCost((int)(h.Agent.healthMax / invItem.initCount * invItem.itemValue - currentHealthCost), "AmmoDispenser"), 0, 9999);
+                        if (healCost > 0)
+                        {
+                            h.AddButton("GiveMechOil", healCost, m =>
+                            {
+                                m.Object.GiveMechOil();
+                                m.StopInteraction();
+                            });
+                        }
+                    }
+                }
+            });
+
+
+
+
+
+
+
 
             Patch<Barbecue>(params2);
+            RogueInteractions.CreateProvider<Barbecue>(h =>
+            {
+                if (h.Helper.interactingFar) return;
+                if (h.Object.burntOut)
+                {
+                    h.SetStopCallback(m => m.Agent.SayDialogue("BarbecueBurntOut"));
+                }
+                else if (h.Object.ora.hasParticleEffect)
+                {
+                    if (h.Agent.inventory.HasItem(VanillaItems.Fud))
+                        h.AddButton("GrillFud", m => m.Object.StartCoroutine(m.Object.Operating(m.Agent, null, 2f, true, "Grilling")));
+                    else h.SetStopCallback(m => m.Agent.SayDialogue("CantGrillFud"));
+                }
+                else
+                {
+                    if (h.Agent.inventory.HasItem(VanillaItems.CigaretteLighter))
+                    {
+                        h.AddButton("LightBarbecue", m =>
+                        {
+                            m.Object.StartFireInObject();
+                            m.StopInteraction();
+                        });
+                    }
+                    else h.SetStopCallback(m => m.Agent.SayDialogue("CantOperateBarbecue"));
+                }
+            });
 
-            RogueInteractions.Factories.Add(new VanillaInteractionFactory()
-                                                .With<Interaction_Barbecue>());
+
+
+
+
+
 
 
 
 
         }
 
+        private static readonly Dictionary<Type, ConstructorInfo> interactionModelConstructors = new Dictionary<Type, ConstructorInfo>();
         public static void PlayfieldObject_Awake(PlayfieldObject __instance)
         {
             if (__instance.GetHook<InteractionModel>() is null)
-                __instance.AddHook<InteractionModel>();
+            {
+                Type myType = __instance.GetType();
+                if (!interactionModelConstructors.TryGetValue(myType, out ConstructorInfo ctor))
+                {
+                    Type modelType = typeof(InteractionModel<>).MakeGenericType(__instance.GetType());
+                    interactionModelConstructors.Add(myType, ctor = modelType.GetConstructor(Type.EmptyTypes));
+                }
+                __instance.AddHook((InteractionModel)ctor?.Invoke(new object[0]));
+            }
         }
         public static bool DetermineButtonsHook(PlayfieldObject __instance)
         {
+			#region Re-implementing base.DetermineButtons()
             __instance.buttons.Clear();
             __instance.buttonPrices.Clear();
             __instance.buttonsExtra.Clear();
+			#endregion
 
             __instance.GetHook<InteractionModel>()?.OnDetermineButtons();
             return false;
         }
         public static bool PressedButtonHook(PlayfieldObject __instance, string buttonText)
         {
+            #region Re-implementing base.PressedButton()
             if (__instance.interactingAgent != null && (__instance.interactingAgent.controllerType != "Keyboard" || (__instance.interactingAgent.controllerType == "Keyboard" && __instance.gc.playerControl.keyCheck(buttonType.Interact, __instance.interactingAgent))) && __instance.interactingAgent.localPlayer)
             {
                 __instance.interactingAgent.mainGUI.invInterface.justPressedInteract = true;
             }
+            #endregion
 
             __instance.GetHook<InteractionModel>()?.OnPressedButton(buttonText);
             return false;
         }
 
-    }
-    internal class Interaction_Barbecue : VanillaInteraction<Barbecue>
-    {
-        public override bool SetupButton()
+        public static bool InteractHook(PlayfieldObject __instance, Agent agent)
         {
-            if (Object.burntOut)
-                return CancelSelf(() => Agent.SayDialogue("BarbecueBurntOut"));
+			#region Re-implementing base.Interact(agent)
+			// PlayfieldObject.Interact
+            __instance.interactingAgent = agent;
+            __instance.hasInteracted = true;
+            __instance.someoneInteracting = true;
+            __instance.DetermineButtons();
 
-            if (Object.ora.hasParticleEffect)
+            if (__instance.playfieldObjectReal != null)
+                __instance.gc.playerAgent.SetCheckUseWithItemsAgain(__instance.playfieldObjectReal);
+
+            float size = !__instance.gc.serverPlayer && __instance.playfieldObjectType == "Agent" ? 1.7f : 1.3f;
+            agent.interactionHelper.boxCollider2D.size = new Vector2(size, size);
+            agent.interactionHelper.interactingLimbo = 0f;
+
+            if (!__instance.gc.serverPlayer && __instance.gc.clientControlling)
+                agent.interactionHelper.clientInteracting = true;
+            if (agent.localPlayer && !__instance.isItem && (agent.statusEffects.hasTrait("RollerSkates") || agent.statusEffects.hasTrait("RollerSkates2")))
             {
-                RogueFramework.Logger.LogWarning($"Current interacting agent is {Agent} ({Agent.agentName}. {Agent.isPlayer})");
-                RogueFramework.Logger.LogWarning($"Has Fud: {Agent.inventory.HasItem("Fud")}");
-                if (Agent.inventory.HasItem("Fud"))
-                    return SetButton("GrillFud");
-                return CancelSelf(() => Agent.SayDialogue("CantGrillFud"));
+                agent.rb.velocity = Vector2.zero;
             }
-            else
+            if (__instance.playfieldObjectAgent != null && __instance.gc.serverPlayer && (__instance.playfieldObjectAgent.movement.curPhysicsType == "Ice" || __instance.playfieldObjectAgent.statusEffects.hasTrait("RollerSkates") || __instance.playfieldObjectAgent.statusEffects.hasTrait("RollerSkates2")))
             {
-                if (Agent.inventory.HasItem("CigaretteLighter"))
-                    return SetButton("LightBarbecue");
-                return CancelSelf(() => Agent.SayDialogue("CantOperateBarbecue"));
+                __instance.playfieldObjectAgent.rb.velocity = Vector2.zero;
             }
+            // ObjectReal.Interact
+            __instance.playerInvDatabase = agent.GetComponent<InvDatabase>();
+			#endregion
+
+            __instance.ShowObjectButtons();
+            return false;
         }
-        public override void OnPressed()
+		// unnecessary?
+        public static bool InteractFarHook(PlayfieldObject __instance, Agent agent)
         {
-            if (ButtonName == "LightBarbecue")
-            {
-                Object.StartFireInObject();
-                StopInteraction();
-            }
-			else if (ButtonName == "GrillFud")
-            {
-                Object.StartCoroutine(Object.Operating(Agent, null, 2f, true, "Grilling"));
-            }
+            // TODO
+            #region Re-implementing base.InteractFar(agent)
+            #endregion
+            return false;
         }
+
     }
 }
