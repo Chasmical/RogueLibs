6f791b6b55e0f3fc8f03517103f04182f307843c
Abbysssal <hillankeyxl2@yandex.ru>
2022-02-26 13:03:42 +0700

    :bookmark: v3.5.0-beta.4

--- RogueLibsCore/RogueLibs.cs
+++ RogueLibsCore/RogueLibs.cs
@@ -39,7 +39,7 @@ namespace RogueLibsCore
         /// <summary>
         ///   <para>Semantic version of RogueLibs that the current assembly is compiled with. Don't use it in BepInPlugin.</para>
         /// </summary>
-        public const string CompiledSemanticVersion = "3.5.0-beta.3";
+        public const string CompiledSemanticVersion = "3.5.0-beta.4";
 
         /// <summary>
         ///   <para>Currently installed and running version of RogueLibs.</para>
