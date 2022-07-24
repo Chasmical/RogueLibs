541fb33546e92d326ce04a38029737266a2fe007
Abbysssal <hillankeyxl2@yandex.ru>
2022-07-24 23:13:49 +0700

    🚚 Move VersionText to a separate file

--- RogueLibsCore/Patches/Patches_Misc.cs
+++ RogueLibsCore/Patches/Patches_Misc.cs
@@ -198,26 +198,4 @@ namespace RogueLibsCore
             __instance.__RogueLibsHooks = null;
         }
     }
-    public class VersionText
-    {
-        internal VersionText(string id) => Id = id;
-        internal VersionText(string id, string? text) : this(id) => preparedText = text;
-        public string Id { get; }
-        private Text? textComponent;
-        private string? preparedText;
-        public string Text
-        {
-            get => textComponent != null ? textComponent.text : preparedText ?? string.Empty;
-            set
-            {
-                if (textComponent != null) textComponent.text = value;
-                else preparedText = value;
-            }
-        }
-        internal void AssignText(Text text)
-        {
-            textComponent = text;
-            text.text = preparedText ?? string.Empty;
-        }
-    }
 }
---
+++ RogueLibsCore/Utilities/VersionText.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.UI;
+
+namespace RogueLibsCore
+{
+    public class VersionText
+    {
+        internal VersionText(string id) => Id = id;
+        internal VersionText(string id, string? text) : this(id) => preparedText = text;
+        public string Id { get; }
+        private Text? textComponent;
+        private string? preparedText;
+        public string Text
+        {
+            get => textComponent != null ? textComponent.text : preparedText ?? string.Empty;
+            set
+            {
+                if (textComponent != null) textComponent.text = value;
+                else preparedText = value;
+            }
+        }
+        internal void AssignText(Text text)
+        {
+            textComponent = text;
+            text.text = preparedText ?? string.Empty;
+        }
+    }
+}
