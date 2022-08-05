b66a92b80d47022ae508cfa41588a4c0b0e27a72
Abbysssal <hillankeyxl2@yandex.ru>
2022-08-06 03:38:12 +0700

    🔖 v3.6.0-rc.3

--- RogueLibsCore/RogueLibs.cs
+++ RogueLibsCore/RogueLibs.cs
@@ -39,7 +39,7 @@ namespace RogueLibsCore
         /// <summary>
         ///   <para>Semantic version of RogueLibs that the current assembly is compiled with. Don't use it in BepInPlugin.</para>
         /// </summary>
-        public const string CompiledSemanticVersion = "3.6.0-rc.2";
+        public const string CompiledSemanticVersion = "3.6.0-rc.3";
         /// <summary>
         ///   <para>Currently installed and running version of RogueLibs.</para>
         /// </summary>
