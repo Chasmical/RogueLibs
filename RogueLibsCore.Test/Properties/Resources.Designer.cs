c85ecc582e0e5026a2305004e8ec565776b5f102
Abbysssal <hillankeyxl2@yandex.ru>
2022-08-09 17:11:57 +0700

    🔧 Update RogueLibsCore.Test resources

--- RogueLibsCore.Test/Properties/Resources.Designer.cs
+++ RogueLibsCore.Test/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace RogueLibsCore.Test.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -106,6 +106,16 @@ namespace RogueLibsCore.Test.Properties {
         internal static byte[] Kamikaze {
             get {
                 object obj = ResourceManager.GetObject("Kamikaze", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] Knocker {
+            get {
+                object obj = ResourceManager.GetObject("Knocker", resourceCulture);
                 return ((byte[])(obj));
             }
         }
