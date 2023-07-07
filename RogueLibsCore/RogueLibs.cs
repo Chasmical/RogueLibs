5e6a3204e5cc1e4573156eaec85adb719a08e2dc
Abbysssal <hillankeyxl2@yandex.ru>
2023-07-07 20:47:35 +0700

    🔨 Update solution files

--- RogueLibsCore/RogueLibs.cs
+++ RogueLibsCore/RogueLibs.cs
@@ -31,15 +31,15 @@ namespace RogueLibsCore
         /// </summary>
         public const string Name = "RogueLibsCore";
 
-        internal const string AssemblyVersion = "3.6.9.0";
+        internal const string AssemblyVersion = "4.0.0.0";
         /// <summary>
         ///   <para>Version of RogueLibs that the current assembly is compiled with.</para>
         /// </summary>
-        public const string CompiledVersion = "3.6.9";
+        public const string CompiledVersion = "4.0.0";
         /// <summary>
         ///   <para>Semantic version of RogueLibs that the current assembly is compiled with. Don't use it in BepInPlugin.</para>
         /// </summary>
-        public const string CompiledSemanticVersion = "3.6.9";
+        public const string CompiledSemanticVersion = "4.0.0-alpha.1";
         /// <summary>
         ///   <para>Currently installed and running version of RogueLibs.</para>
         /// </summary>
